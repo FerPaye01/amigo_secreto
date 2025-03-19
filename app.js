161afc55ec3aace525dc7c7c0fa9c25082ecb3fe
Oscar Fernando Paye Cahui <ferpaye438@gmail.com>
2025-03-18 19:57:35 -0500

    Update

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
 let personasDisponibles = [];
 let amigosSecretosDisponibles = [];
 let personasSorteadas = [];
